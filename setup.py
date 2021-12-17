9567176531f90bb5fb8ca47f5a6bb230dd4a95e7
rc-crafter <93624337+Trojaner4@users.noreply.github.com>
2021-12-17 19:16:36 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import os
+os.system('wget https://github.com/Trojaner4/minetestpi4/archive/refs/heads/main.zip')
+os.system('unzip main.zip')
+os.system('unzip minetestpi4-main/minetest.zip')
+print("finished!")
